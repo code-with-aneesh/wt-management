94181e7e4ecd9dbe382bbca9f7f59927bc6dd25b
Aneesh Angane <anesh.angane@gmail.com>
2025-06-07 00:59:12 +0530

    fix 2 for vercel and pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,6 +38,7 @@
 
 <svelte:head>
   {@html webManifestLink}
+  <link rel="manifest" href="/manifest.webmanifest" />
   <link
     href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
     rel="stylesheet"
